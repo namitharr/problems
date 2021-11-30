039e4dae0d17808cdfc2f6cbc909860ce951a5b4
Namitha Reval <namitha.reval@gmail.com>
2021-11-30 11:46:46 +0530

    2021.11.30 •l_1880

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+### Leetcode
+
 | Problem | Solution |
 | ------- | -------- |
 | [2089. Find Target Indices After Sorting Array](https://leetcode.com/problems/find-target-indices-after-sorting-array/) | [Java](src/leetcode/TargetIndicesInArray.java) |
+| [1880. Check if Word Equals Summation of Two Words](https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/) | [Java](src/leetcode/WordIsSumOfTwoWords.java)
---
+++ src/leetcode/WordIsSumOfTwoWords.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+// 1880. https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/
+public class WordIsSumOfTwoWords {
+	public static void main(String[] args) {
+		System.out.println(new WordIsSumOfTwoWords().isSumEqual("acb", "cba", "cdb"));
+		System.out.println(new WordIsSumOfTwoWords().isSumEqual("aaa", "a", "aab"));
+		System.out.println(new WordIsSumOfTwoWords().isSumEqual("aaa", "a", "aaaa"));
+	}
+
+	public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+		int carry = 0;
+		for (int first = firstWord.length() -1, second = secondWord.length() - 1, target = targetWord.length() - 1; target >= 0;
+		     first--, second--, target--) {
+
+			int sum = 0;
+			if (first >= 0) sum += firstWord.charAt(first) - 'a';
+			if (second >= 0) sum += secondWord.charAt(second) - 'a';
+
+			sum += carry;
+			carry = sum / 10;
+			if (targetWord.charAt(target) - 'a' != sum % 10) return false;
+		}
+
+		return carry == 0;
+	}
+}
