0635aae76c501d78bed6c7d9b7790b908de96058
Namitha Reval <namitha.reval@gmail.com>
2021-12-01 12:58:34 +0530

    2021.12.01 •L2085

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 | Problem | Solution |
 | ------- | -------- |
 | [2089. Find Target Indices After Sorting Array](https://leetcode.com/problems/find-target-indices-after-sorting-array/) | [Java](src/leetcode/TargetIndicesInArray.java) |
-| [1880. Check if Word Equals Summation of Two Words](https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/) | [Java](src/leetcode/WordIsSumOfTwoWords.java)
+| [2085. Count Common Words With One Occurrence](https://leetcode.com/problems/count-common-words-with-one-occurrence/) | [Java](src/leetcode/L2085.java) |
+| [1880. Check if Word Equals Summation of Two Words](https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/) | [Java](src/leetcode/WordIsSumOfTwoWords.java) |
---
+++ src/leetcode/L2085.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 2085. Count Common Words With One Occurrence
+// https://leetcode.com/problems/count-common-words-with-one-occurrence/
+public class L2085 {
+	public static void main(String[] args) {
+		System.out.println(new L2085().countWords(
+						stringArray("leetcode", "is", "amazing", "as", "is"),
+						stringArray("amazing", "leetcode", "is")
+		));
+		System.out.println(new L2085().countWords(
+						stringArray("b", "bb", "bbb"),
+						stringArray("a", "aa", "aaa")
+		));
+		System.out.println(new L2085().countWords(
+						stringArray("a", "ab"),
+						stringArray("a", "a", "a", "ab")
+		));
+	}
+
+	public static String[] stringArray(String... strings) {
+		return strings;
+	}
+
+	public int countWords(String[] words1, String[] words2) {
+		Map<String, Integer> map1 = new HashMap<>();
+		for (String word : words1) map1.put(word, map1.getOrDefault(word, 0) + 1);
+
+		Map<String, Integer> map2 = new HashMap<>();
+		for (String word : words2) map2.put(word, map2.getOrDefault(word, 0) + 1);
+
+		int count = 0;
+		for (String word : words1)
+			if (map1.containsKey(word) && map2.containsKey(word) && map1.get(word) == 1 && map2.get(word) == 1)
+				count++;
+
+		return count;
+	}
+}
